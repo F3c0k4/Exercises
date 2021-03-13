daac6c7d9065d7ec54c7ef4b762fc9d1bb8b57e8
f3c0k4 <zagferenc@gmail.com>
2021-03-13 23:18:03 +0100

    Exercises for module 8

---
+++ Exercises_08/Exercises01-02/Exercise01-02.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type user struct {
+	First string
+	Age   int
+}
+
+func main() {
+	u1 := user{
+		First: "James",
+		Age:   32,
+	}
+
+	u2 := user{
+		First: "Moneypenny",
+		Age:   27,
+	}
+
+	u3 := user{
+		First: "M",
+		Age:   54,
+	}
+
+	users := []user{u1, u2, u3}
+
+	fmt.Println(users)
+	fmt.Println("\n\tExercise 2\n")
+	//Exercise 1
+
+	u, err := json.Marshal(users)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(u))
+
+	fmt.Println("\n\tExercise 2\n")
+	//Exercise 2
+	s := `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
+	fmt.Println(s)
+	var v []byte
+	json.Unmarshal([]byte(s), &v)
+	fmt.Println(string(v))
+}
